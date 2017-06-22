5583994b91d26205e51dd8082eb8d87018683e5c
zhangqiangoffice <zhangqiangoffice@163.com>
2017-06-22 23:56:36 +0800

    v1.0.1

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const electron = require('electron')
+const {BrowserWindow, app, globalShortcut} = require('electron')
 // Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+// const app = electron.app
+// // Module to create native browser window.
+// const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -13,8 +14,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
+  console.log('createWindow')
   mainWindow = new BrowserWindow({width: 800, height: 600})
+  // let child = new BrowserWindow({parent: mainWindow})
 
+  // child.show()
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -27,22 +31,45 @@ function createWindow () {
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
+    console.log('closed');
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  globalShortcut.register('CmdOrCtrl+z', () => {
+    app.quit()
+  })
 }
+
+app.on('before-quit', (event) => {
+  console.log('before-quit')
+})
+app.on('will-quit', () => {
+  console.log('will-quit')
+  event.preventDefault()
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
+app.on('browser-window-focus', () => {
+  console.log('browser-window-focus');
+  console.log(app.getLocale());
+})
+
+app.on('browser-window-blur', () => {
+  console.log('browser-window-blur');
+})
+
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
+  console.log('window-all-closed');
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -51,6 +78,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
+  console.log('activate')
   if (mainWindow === null) {
     createWindow()
   }
